c6c72315d93a4f450683a446c12788fd08f64238
barril01 <88594469+barril01@users.noreply.github.com>
2023-09-30 14:12:11 -0400

    Module 5 complete and submitted

--- Module_5/matlab/Problem_2_39.m
+++ Module_5/matlab/Problem_2_39.m
@@ -1,5 +1,7 @@
 % Probelem 2-39
 
+clc,clear,close all
+
 T = 0:0.01:10;
 
 for t = 1:length(T)
@@ -13,16 +15,24 @@ posIdx = (K_bound>0);
 K_bound(~posIdx) = [];
 T(~posIdx) = [];
 
-plot(T,K_bound,'LineWidth',2)
+area(T(K_bound>1),K_bound(K_bound>1),0,'EdgeColor','none','FaceColor','g')
 
+
+grid on
 xlim([-1,10])
 ylim([-1,10])
 
 hold on
-area(T(K_bound>1),K_bound(K_bound>1),0,'EdgeColor','none','FaceColor','g')
+area([0 T(1)],[500 500],0,'EdgeColor','none','FaceColor','g')
+plot(T,K_bound,'b','LineWidth',2)
+plot([0,T(end)],[0,0],'b','LineWidth',2)
+plot([0,0],[0,K_bound(1)],'b','LineWidth',2)
 
 xlim([-1,10])
 ylim([-1,10])
+
+yline(1,'-','Limit')
+xline(T(1),'-','Limit')
 
 xlabel('T')
 ylabel('K')
